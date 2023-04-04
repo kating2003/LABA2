8c3696ebb249d9cb107a4065bef9a8de33869e6c
Maxim <maxim@debian-BULLSEYE-live-builder-AMD64>
2023-04-04 17:48:52 +0300

    Better Mk1

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-using namespace std;
-
 int main()
 {
     string a;
-    cin >> a;
-  cout << "Hello world from " << a << endl;
+   std::cin >> a;
+ std::cout << "Hello world from " << a << endl;
 }
 
 
