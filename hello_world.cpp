58b65e4edb4a99085ed0f2bd64c02b2d3d86febd
kating2003 <60068727+kating2003@users.noreply.github.com>
2023-04-04 18:51:49 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main()
 {
-    string a; // defining variable 
-   std::cin >> a; // input variable
- std::cout << "Hello world from " << a << endl; //output message
+    string a; // Определение переменных.
+   std::cin >> a; // Ввод переменных.
+ std::cout << "Hello world from " << a << endl; //Вывод сообщения.
 }
 
 
